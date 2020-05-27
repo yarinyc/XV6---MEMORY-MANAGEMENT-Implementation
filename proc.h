ebd0c2ffc02e8cb411e30220bcf72894ac5cdd5f
yarinyc <yarinyc@gmail.com>
2020-05-27 14:33:48 +0300

    initial

--- proc.h
+++ proc.h
@@ -34,23 +34,30 @@ struct context {
 
 enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+struct meta_data{
+  uint page_id;
+  uint present;
+  uint offset;
+};
+
 // Per-process state
 struct proc {
-  uint sz;                     // Size of process memory (bytes)
-  pde_t* pgdir;                // Page table
-  char *kstack;                // Bottom of kernel stack for this process
-  enum procstate state;        // Process state
-  int pid;                     // Process ID
-  struct proc *parent;         // Parent process
-  struct trapframe *tf;        // Trap frame for current syscall
-  struct context *context;     // swtch() here to run process
-  void *chan;                  // If non-zero, sleeping on chan
-  int killed;                  // If non-zero, have been killed
-  struct file *ofile[NOFILE];  // Open files
-  struct inode *cwd;           // Current directory
-  char name[16];               // Process name (debugging)
+  uint sz;                        // Size of process memory (bytes)
+  pde_t* pgdir;                   // Page table
+  char *kstack;                   // Bottom of kernel stack for this process
+  enum procstate state;           // Process state
+  int pid;                        // Process ID
+  struct proc *parent;            // Parent process
+  struct trapframe *tf;           // Trap frame for current syscall
+  struct context *context;        // swtch() here to run process
+  void *chan;                     // If non-zero, sleeping on chan
+  int killed;                     // If non-zero, have been killed
+  struct file *ofile[NOFILE];     // Open files
+  struct inode *cwd;              // Current directory
+  char name[16];                  // Process name (debugging)
   //Swap file. must initiate with create swap file
-  struct file *swapFile;      //page file
+  struct file *swapFile;          //page file
+  struct meta_data meta_data[32]; //page offsets in swap file
 };
 
 // Process memory is laid out contiguously, low addresses first:
